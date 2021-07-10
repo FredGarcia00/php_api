05aef6b9175340fdc92be8e8f0cb181912536ecb
Fred Garcia <Fred@Garcia.com>
2021-07-10 13:52:25 -0700

    first commit

---
+++ models/Post.php
@@ -0,0 +1,92 @@
+<?php
+
+class Post {
+    // DB 
+    private $conn;
+    private $table = 'posts';
+
+    // Post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    //Constructor with DB
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Get Posts
+    public function read() {
+        //Create query 
+        $query = 'SELECT 
+                  c.name as category_name,
+                  p.id,
+                  p.category_id,
+                  p.title,
+                  p.body,
+                  p.author,
+                  p.created_at
+                  FROM
+                  ' . $this->table. ' p
+                  LEFT JOIN
+                  categories c 
+                  ON 
+                  p.category_id = c.id
+                  ORDER BY
+                  p.created_at 
+                  DESC';
+
+                  //Prepare statement
+                  $stmt = $this->conn->prepare($query);
+
+                  //Execute query 
+                  $stmt->execute();
+
+                  return $stmt;
+                  
+    }
+
+    //Get single post
+    public function read_single() {
+        $query = 'SELECT 
+                  c.name as category_name,
+                  p.id,
+                  p.category_id,
+                  p.title,
+                  p.body,
+                  p.author,
+                  p.created_at
+                  FROM
+                  ' . $this->table. ' p
+                  LEFT JOIN
+                  categories c 
+                  ON 
+                  p.category_id = c.id
+                  WHERE
+                  p.id = ?
+                  LIMIT 0,1';
+
+                //Prepare statement
+                $stmt = $this->conn->prepare($query);
+
+                //Bind ID
+                $stmt->bindParam(1, $this->id);
+
+                //Execute query 
+                $stmt->execute();
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                //set properties
+                $this->title = $row['title'];
+                $this->title = $row['body'];
+                $this->title = $row['author'];
+                $this->title = $row['category_id'];
+                $this->title = $row['category_name'];
+
+    }
+}
